f18188ae553df39d631044027a3be711b378709f
VidadiMurad <vidad0murad@gmail.com>
2026-04-16 18:04:40 +0400

    Added Variables

--- Enter/src/App.java
+++ Enter/src/App.java
@@ -1,9 +1,19 @@
 // Compound Interest Calculator
 
+// Library for get data from user
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+    System.out.println("Compound Interest Calculator");
+    
+    // Variables
+    double pMoney;
+    double rPercent;
+    double yPercent;
+    double tYears;
+
+    
+
     }
 }
